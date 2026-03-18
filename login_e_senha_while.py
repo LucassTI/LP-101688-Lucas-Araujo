a32e057c35b5c4bf0c6797316f371e0cba0e2919
LucassTI <lucasaraujo3891@gmail.com>
2026-03-18 14:31:51 -0300

    Add files via upload

---
+++ login_e_senha_while.py
@@ -0,0 +1,21 @@
+import os
+
+os. system ("cls")
+
+
+
+while True:
+        login = input("Digite seu login:  ")
+        senha = input ("Digite sua senha: ")
+        LOGIN = "joaoraimundo"
+        SENHA = "tacomazarhoje"
+        if login == LOGIN and senha == SENHA:
+            print("Acesso permitido!")
+            break
+        else:
+            print("Acesso negado!")
+            
+    
+    
+    
+    
