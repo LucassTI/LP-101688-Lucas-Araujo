9f9d714b5e36dbd57e88e6af39789e4193d7374f
LucassTI <lucasaraujo3891@gmail.com>
2026-05-05 16:46:40 -0300

    Add files via upload

---
+++ 8.revisao_classes.py
@@ -0,0 +1,33 @@
+import os
+from dataclasses import dataclass
+os.system("cls")
+
+
+@dataclass
+@dataclass
+class Funcionario:
+    nome: str
+    matricula: str
+    turno: str
+    setor: str
+
+    def mostrar_dados(self):
+        print(f"Nome do Funcionário: {self.nome}")
+        print(f"Matrícula: {self.matricula}")
+        print(f"Setor: {self.setor}")
+        print(f"Turno: {self.turno}")
+
+lista_funcionarios = []
+
+with open ('lista.funcionarios.csv', 'r') as arquivo:
+    for linha in arquivo:
+        nome, matricula, turno, setor = linha.strip().split(',')
+        lista_funcionarios.append(Funcionario(
+            nome=nome,
+            matricula=matricula,
+            setor=setor,
+            turno=turno,
+        ))
+
+        for funcionarios in lista_funcionarios:
+            funcionarios.mostrar_dados()
