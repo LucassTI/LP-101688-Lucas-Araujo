6462d507aaf008f5ce0742e20c23434b50a25cce
LucassTI <lucasaraujo3891@gmail.com>
2026-05-06 20:37:45 -0300

    Update 8.revisao_classes.py

--- 8.revisao_classes.py
+++ 8.revisao_classes.py
@@ -4,7 +4,7 @@ os.system("cls")
 
 
 @dataclass
-@dataclass
+
 class Funcionario:
     nome: str
     matricula: str
@@ -20,14 +20,15 @@ class Funcionario:
 lista_funcionarios = []
 
 with open ('lista.funcionarios.csv', 'r') as arquivo:
-    for linha in arquivo:
-        nome, matricula, turno, setor = linha.strip().split(',')
-        lista_funcionarios.append(Funcionario(
-            nome=nome,
-            matricula=matricula,
-            setor=setor,
-            turno=turno,
+        for linha in arquivo:
+            nome, matricula, turno, setor = linha.strip().split(',')
+            funcionarios = (Funcionario(
+                nome=nome,
+                matricula=matricula,
+                setor=setor,
+                turno=turno,
         ))
+        lista_funcionarios.append(funcionarios)
 
         for funcionarios in lista_funcionarios:
             funcionarios.mostrar_dados()
